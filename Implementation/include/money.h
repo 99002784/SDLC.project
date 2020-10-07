68612dbb66951c181a089e8b7e99faac58a9e90d
99002784 <72460456+99002784@users.noreply.github.com>
2020-10-07 06:30:59 +0530

    Create money.h

---
+++ Implementation/include/money.h
@@ -0,0 +1,15 @@
+#ifndef __MONEY_H__
+#define __MONEY_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void acceptPassword(int PIN);
+int with_amt(int withdraw,int amt);
+int deposits_amt(int deposit,int amt);
+ int to_send(int send,int amt);
+
+
+
+
+#endif
